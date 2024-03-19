3f8e67228b925c2579f7e7167d128c7c243c00f2
Paramet Prasongngern <157900340+Paramet02@users.noreply.github.com>
2024-03-19 19:35:58 +0700

    Add files via upload

---
+++ smart plug - noed red/src/main.cpp
@@ -0,0 +1,92 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <PubSubClient.h>
+
+const char* ssid = ""; // wifi ssid
+const char* password = "";
+const char* mqtt_server = ""; // mosquitto server url
+
+#define relay 2
+int SW_relay = 0;
+
+#define button 4
+int buttonStatus = 0;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  if (strcmp(topic, "/ThinkIOT/Subscribe") == 0) {
+    if (payload[0] == '1') {
+      digitalWrite(relay, HIGH);
+      Serial.println("Status : on");
+    } else {
+      digitalWrite(relay, LOW);
+      Serial.println("Status : off");
+    }
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str())) {
+      Serial.println("Connected");
+      client.publish("/ThinkIOT/Publish", "Welcome");
+      client.subscribe("/ThinkIOT/Subscribe");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(relay, OUTPUT);
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  // Your other code here
+}
